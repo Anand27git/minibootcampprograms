f2d5f70309a54b81b0283011453a3bb23c7d8191
Anand27git <anandkumar256k@gmail.com>
2021-05-11 11:45:22 +0530

    randomdigitno commit success

---
+++ randomdigitno.sh
@@ -0,0 +1,28 @@
+#! /bin/bash -x
+for (( i = 0; i < 10; i++ )); do
+number=$((RANDOM%899+100))
+arr[((i))]=$number
+done
+echo ${arr[@]}
+# variables to track min and max
+max=${arr[0]};
+min=${arr[0]};
+max2=0;
+min2=0;
+# calculating 2nd max and 2nd min
+for (( i = 0; i < 10; i++ )); do
+# checking 2nd max
+if [[ ${arr[i]} -gt $max ]]; then
+max2=$max
+max=${arr[i]}
+fi
+# checking 2nd min
+if [[ ${arr[i]} -lt $min ]]; then
+min2=$min
+min=${arr[i]}
+fi
+done
+echo "Max:$max, Min:$min"
+echo "2nd max:$max2"
+echo "2nd min:$min2"
+
